68de93659b6592af74678a18ad8b02334993c3ae
Pratham072 <132221235+Pratham072@users.noreply.github.com>
2023-12-06 11:48:56 +0530

    Update binarySearch.cpp

--- structure/binarySearch.cpp
+++ structure/binarySearch.cpp
@@ -26,3 +26,69 @@ int main()
                   << ind << endl;
     return 0;
 }
+
+/*
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+
+int partition(std::vector<int>& arr, int low, int high) {
+    
+    int randomIndex = low + rand() % (high - low + 1);
+
+  
+    std::swap(arr[randomIndex], arr[high]);
+
+    int pivot = arr[high]; // Pivot element
+    int i = low - 1; // Index of smaller element
+
+    for (int j = low; j < high; ++j) {
+        if (arr[j] < pivot) {
+            ++i;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+
+void quicksort(std::vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        // Recursively sort the sub-arrays
+        quicksort(arr, low, pivotIndex - 1);
+        quicksort(arr, pivotIndex + 1, high);
+    }
+}
+
+
+void printArray(const std::vector<int>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+   
+    srand(time(0));
+
+    std::vector<int> arr = {12, 4, 5, 6, 7, 3, 1, 15};
+
+    std::cout << "Original array: ";
+    printArray(arr);
+
+    int size = arr.size();
+    quicksort(arr, 0, size - 1);
+
+    std::cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
+*/
